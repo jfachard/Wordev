b46b67d93766483ac93930181dc6968b107b3d85
Achard Jean-Francis <jean-francis.achard@epitech.eu>
2026-05-08 14:57:46 +0200

    feat: integrate vue3-toastify for game notifications and update styles

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { MotionPlugin } from '@vueuse/motion'
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
 
 import GameGrid from './components/GameGrid.vue'
 import GameHint from './components/GameHint.vue'
@@ -9,6 +10,7 @@ import Navbar from './components/Navbar.vue'
 import ThemeToggle from './components/ThemeToggle.vue'
 
 import './index.css'
+import 'vue3-toastify/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
@@ -18,6 +20,11 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(MotionPlugin)
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+  position: 'top-center',
+  pauseOnHover: true,
+} as ToastContainerOptions)
 
 app.component('GameGrid', GameGrid)
 app.component('GameHint', GameHint)
